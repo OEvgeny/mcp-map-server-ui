11ff5e0c17be748d5f4bd1ae180a69573627e7e1
Eugene <EOlonov@gmail.com>
2026-02-13 18:03:24 +0000

    Better fs and cleanup

--- esm/module/fs.js
+++ esm/module/fs.js
@@ -1,7 +1,17 @@
 
 async function readFile(path) {
-  const url = new URL(path, import.meta.url)
-  return await (await fetch(url)).text();
+  const url = new URL(path, import.meta.url);
+
+  if (url.protocol === "file:") { // assume node:fs support as we can't fetch file: protocol anyways
+    const { readFile: nodeReadFile } = await import("runtime:fs/promises");
+    const { fileURLToPath } = await import("runtime:url");
+    return await nodeReadFile(fileURLToPath(url), "utf8");
+  } else {
+    const res = await fetch(url);
+    if (!res.ok) throw new Error(`readFile: ${res.status} ${res.statusText} (${url})`, { cause: res });
+    return await res.text();
+  }
 }
 
-export default { readFile }
+export default { readFile };
+export { readFile };
